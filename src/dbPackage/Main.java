9aac7423a944b903d51166ebbecbe822e15d2bb5
jaeyoung0513 <rjykab@gmail.com>
2024-09-27 15:21:11 +0900

    print문 제거

--- src/dbPackage/Main.java
+++ src/dbPackage/Main.java
@@ -2,8 +2,6 @@ package dbPackage;
 
 public class Main {
     public static void main(String[] args) {
-    	System.out.print("ㅎㅇ ㅎㅎ");
-    	System.out.println("나도 연습");
         DBManager dbManager = new DBManager(); // DBManager 이름이 정확해야 함
         dbManager.initDBConnect();
 
